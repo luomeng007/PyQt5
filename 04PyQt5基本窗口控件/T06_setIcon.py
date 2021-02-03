d44917989076e00bcd783064ffc9c79b8d2251aa
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2021-02-03 10:07:53 +0100

    Update T06_setIcon.py

--- 04PyQt5基本窗口控件/T06_setIcon.py
+++ 04PyQt5基本窗口控件/T06_setIcon.py
@@ -6,15 +6,16 @@ software: PyCharm
 
 Description:
     set icon for window
+    we introduce two methods here. The second is a simplified one
 """
 import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
-class Icon(QWidget):
+class IconMethod01(QWidget):
     def __init__(self, parent=None):
-        super(Icon, self).__init__(parent)
+        super(IconMethod01, self).__init__(parent)
         self.initUI()
 
     def initUI(self):
@@ -22,8 +23,25 @@ class Icon(QWidget):
         self.setWindowIcon(QIcon('./icon/cat.ico'))
 
 
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     icon = IconMethod01()
+#     icon.show()
+#     sys.exit(app.exec_())
+
+
+# ======================================================================================================================
+
+
+class IconMethod02(QWidget):
+    def __init__(self, parent=None):
+        super(IconMethod02, self).__init__(parent)
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowIcon(QIcon('./icon/cat.ico'))
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    icon = Icon()
+    icon = IconMethod02()
     icon.show()
     sys.exit(app.exec_())
